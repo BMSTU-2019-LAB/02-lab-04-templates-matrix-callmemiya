846403e82ba6c53ba53c16c83df2b29dbe055640
callmemiya <mari.zabrodina.01@mail.ru>
2020-04-28 17:33:50 +0300

    test1

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -12,28 +12,26 @@ using namespace std;
 
 template<class T>
 class Matrix{
-    static_assert(std::is_arithmetic<T>::value,"Not arithmetic type");
+    static_assert(std::is_arithmetic<T>::value, "Not arithmetic type");
     int rows;
     int columns;
     T** m;
-    
 public:
     Matrix(){
-        rows=0;
-        columns=0;
-        m=nullptr;
+        rows = 0;
+        columns = 0;
+        m = nullptr;
     }
-    
-    Matrix(int rows,int columns){
-        this->rows=rows;
-        this->columns=columns;
-        m=new T*[rows];
-        for (int i=0;i<rows;i++){
-            m[i]=new T[columns];
+    Matrix(int rows, int columns){
+        this->rows = rows;
+        this->columns = columns;
+        m = new T*[rows];
+        for (int i = 0; i < rows; i++){
+            m[i] = new T[columns];
         }
-        for (int i=0;i<rows;i++){
-            for (int j=0;j<columns;j++){
-                m[i][j]=0;
+        for (int i = 0; i < rows; i++){
+            for (int j = 0 ; j < columns; j++){
+                m[i][j] = 0;
             }
         }
     };
@@ -43,34 +41,33 @@ public:
     }
     
     Matrix(const Matrix& M){
-        columns=M.columns;
-        rows=M.rows;
-        m=new T*[rows];
-        for (int i=0;i<rows;i++){
-            m[i]=new T[columns];
+        columns = M.columns;
+        rows = M.rows;
+        m = new T*[rows];
+        for (int i = 0; i < rows; i++){
+            m[i] = new T[columns];
         }
-        for (int i=0;i<rows;i++){
-            for (int j=0;j<columns;j++){
-                m[i][j]=M[i][j];
+        for (int i = 0 ;i < rows; i++){
+            for (int j=0; j < columns; j++){
+                m[i][j] = M[i][j];
             }
         }
     }
     
-    Matrix & operator=(const Matrix& L){
-           columns=L.columns;
-           rows=L.rows;
-           m=new T*[rows];
-           for (int i=0;i<rows;i++){
-               m[i]=new T[columns];
+    Matrix & operator =(const Matrix& L){
+           columns = L.columns;
+           rows = L.rows;
+           m = new T*[rows];
+           for (int i = 0; i < rows; i++){
+               m[i] = new T[columns];
            }
-           for (int i=0;i<rows;i++){
-               for (int j=0;j<columns;j++){
-                   m[i][j]=L[i][j];
+           for (int i = 0; i < rows; i++){
+               for (int j=0 ; j < columns; j++){
+                   m[i][j] = L[i][j];
                }
            }
            return *this;
     }
-    
     int Rows() const{
         return rows;
     }
@@ -80,77 +77,79 @@ public:
     }
     
     Matrix operator+(const Matrix& H) const{
-        if (columns!=H.columns || rows!=H.rows) {
+        if (columns != H.columns || rows != H.rows) {
             Matrix<T> error;
             return error;
         }
-        Matrix<T> sum(rows,columns);
-        for (int i=0;i<rows;i++){
-            for (int j=0;j<columns;j++){
-                sum.m[i][j]=m[i][j]+H.m[i][j];
+        Matrix<T> sum(rows, columns);
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j<columns; j++){
+                sum.m[i][j] = m[i][j]+H.m[i][j];
             }
         }
         return sum;
     }
     
     Matrix operator-(const Matrix& H) const{
-        if (columns!=H.columns || rows!=H.rows) {
+        if (columns != H.columns || rows != H.rows) {
             Matrix<T> error;
             return error;
         }
-        Matrix<T> raz(rows,columns);
-        for (int i=0;i<rows;i++){
-            for (int j=0;j<columns;j++){
-                raz.m[i][j]=m[i][j]-H.m[i][j];
+        Matrix<T> raz(rows, columns);
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){
+                raz.m[i][j] = m[i][j]-H.m[i][j];
             }
         }
         return raz;
     }
     
     Matrix operator*(const Matrix& F){
-        if (columns!=F.rows){
+        if (columns != F.rows){
             Matrix<T> error;
             return error;
         }
-        Matrix<T> proiz(rows,F.columns);
-        for (int i=0;i<rows;i++){
-            for (int j=0;j<F.columns;j++){
+        Matrix<T> proiz(rows, F.columns);
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < F.columns; j++){
                 for (int k = 0; k < columns; k++)
                     proiz[i][j] += m[i][k] * F.m[k][j];
                 }
             }
-         for (int i=0;i<rows;i++){
-            for (int j=0;j<F.columns;j++){
-                if (abs(proiz[i][j])<10*std::numeric_limits<double>::epsilon()) proiz[i][j]=0;
-                if (abs(abs(proiz[i][j])-round(proiz[i][j]))<10*std::numeric_limits<double>::epsilon()) proiz[i][j]=round(proiz[i][j]);
+         for (int i = 0; i < rows; i++){
+            for (int j = 0; j < F.columns; j++){
+                if (abs(proiz[i][j])<10*std::numeric_limits<double>::epsilon()) proiz[i][j] = 0;
+                if (abs(abs(proiz[i][j])-round(proiz[i][j]))<10*std::numeric_limits<double>::epsilon()) proiz[i][j] = round(proiz[i][j]);
             }
         }
         return proiz;
     }
     
-    bool operator!=(const Matrix& N) const{
-        for (int i=0;i<rows;i++){
-            for (int j=0;j<columns;j++){
-                if (m[i][j]!=N[i][j]);
-                else return false;
+    bool operator != (const Matrix& N) const{
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){
+                if (m[i][j] != N[i][j]) {};
+                else
+                    return false;
             }
         }
         return true;
     }
     
-    bool operator==(const Matrix& O) const{
-           for (int i=0;i<rows;i++){
-               for (int j=0;j<columns;j++){
-                   if (m[i][j]==O[i][j]);
-                   else return false;
+    bool operator == (const Matrix& O) const{
+           for (int i = 0; i < rows; i++){
+               for (int j = 0; j < columns; j++){
+                   if (m[i][j] == O[i][j]);
+                   else
+                       return false;
                }
            }
            return true;
        }
     
     ~Matrix(){
-        if (m!=nullptr){
-            for (int i=0;i<rows;i++){
+        if (m != nullptr){
+            for (int i = 0; i < rows; i++){
                 delete[] m[i];
             }
             delete[] m;
@@ -158,55 +157,55 @@ public:
     }
     
     Matrix Inverse() const{
-        if (rows!=columns){
+        if (rows != columns){
             Matrix<T> error;
             return error;
         }
         Matrix<T> Turn(rows,2*columns);
-        for (int i=0;i<rows;i++){
-            for (int j=0;j<columns;j++){
-                Turn[i][j]=m[i][j];
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){
+                Turn[i][j] = m[i][j];
             }
         }
-        for (int i=0;i<rows;i++){
-            for (int j=columns;j<2*columns;j++){
-                if (i==j-columns) Turn[i][j]=1;
+        for (int i = 0; i < rows; i++){
+            for (int j = columns; j < 2*columns; j++){
+                if (i == j-columns) Turn[i][j] = 1;
             }
         }
-        double l=0;
-        double n=0;
-        for (int j=0;j<2*columns;j++){
-            for (int i=0;i<rows;i++){
-                if (i==j){
-                    if (Turn[i][j]==0){
-                        int r=i;
-                        while (Turn[r][j]==0){
+        double l = 0;
+        double n = 0;
+        for (int j = 0; j < 2*columns; j++){
+            for (int i = 0; i < rows; i++){
+                if (i == j){
+                    if (Turn[i][j] == 0){
+                        int r = i;
+                        while (Turn[r][j] == 0){
                             r++;
                         }
-                        for (int q=0;q<2*columns;q++){
-                            swap(Turn[i][q],Turn[r][q]);
+                        for (int q = 0; q < 2*columns; q++){
+                            std::swap(Turn[i][q],Turn[r][q]);
                         }
                     }
-                        l=Turn[i][j];
-                        for (int k=0;k<2*columns;k++){
-                            if (Turn[i][k]!=0){
-                                Turn[i][k]=Turn[i][k]/l;
+                        l = Turn[i][j];
+                        for (int k = 0; k < 2*columns; k++){
+                            if (Turn[i][k] != 0){
+                                Turn[i][k] = Turn[i][k]/l;
                             }
                         }
                 }
-                if (i>j){
-                    n=Turn[i][j]/Turn[j][j];
-                    for (int k=0;k<2*columns;k++){
+                if (i > j){
+                    n = Turn[i][j]/Turn[j][j];
+                    for (int k = 0; k < 2*columns; k++){
                         Turn[i][k]-=Turn[j][k]*n;
                     }
                 }
             }
         }
-        for (int j=columns-1;j>=0;j--){
-            for (int i=rows-1;i>=0;i--){
-                if (i<j){
-                    if (Turn[j][j]!=0){n=Turn[i][j]/Turn[j][j];
-                        for (int k=0;k<2*columns;k++){
+        for (int j = columns-1; j >= 0; j--){
+            for (int i = rows-1; i >= 0; i--){
+                if (i < j){
+                    if (Turn[j][j] != 0){n = Turn[i][j]/Turn[j][j];
+                        for (int k = 0;k < 2*columns; k++){
                             Turn[i][k]-=Turn[j][k]*n;
                         }
                     }
@@ -214,9 +213,9 @@ public:
             }
         }
         Matrix<T> F(rows,columns);
-        for (int i=0;i<rows;i++){
-            for (int j=0;j<columns;j++){
-                F[i][j]=Turn[i][j+columns];
+        for (int i = 0;i < rows; i++){
+            for (int j = 0; j < columns; j++){
+                F[i][j] = Turn[i][j+columns];
             }
         }
     return F;
@@ -232,7 +231,9 @@ bool operator==(const Matrix<double> &P, const Matrix<double> &Q){
     if (std::is_floating_point<T>::value){
         for (int i=0;i<P.Rows();i++){
             for (int j=0;j<P.Cols();j++){
-                if (abs(P[i][j]-Q[i][j])>std::numeric_limits<double>::epsilon()) return false;
+                if (abs(P[i][j]-Q[i][j])>std::numeric_limits<double>::epsilon()){
+                    return false;
+                }
             }
         }
         return true;
